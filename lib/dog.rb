5fde98191ab77f94dbcbb5890e2526954b1001c7
Justin Daniel <justinddaniel@gmail.com>
2017-10-11 22:29:31 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -47,7 +47,7 @@ class Dog
     DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 
-  def self.create(name, breed)
+  def self.create(hash)
     dog = self.new(name, breed)
     dog.save
     dog
