69b47067b1999cfac9be40ad8cab3d3b1dd1674b
Justin Daniel <justinddaniel@gmail.com>
2017-10-11 23:09:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -74,10 +74,10 @@ class Dog
   end
 
   def self.find_or_create_by(name:, breed:)
-    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = '#{name}' AND breed = '#{breed}'")
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = '#{name}' AND breed = '#{breed}'") #passing in hash key/value pair.
       if !dog.empty?
         dog_data = dog[0]
-        dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+        dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2]) #another key/value pair pass, where dog_data[i] is value. 
       else
         dog = self.create(name: name, breed: breed)
     end
